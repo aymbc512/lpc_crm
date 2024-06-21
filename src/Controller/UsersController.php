d91dae37683a1b9e0fc826bc47ed8493129334aa
aymbc512 <aymbc512@gmail.com>
2024-06-21 13:09:51 +0900

    0621userscontroller編集村井

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -75,6 +75,8 @@ class UsersController extends AppController
     $this->set(compact('creators', 'updaters'));
   
    $this->fetchTable('SelectionLists');
+   //$recentSelectionLists = $this -> SelectionLists ->find(all);
+  // $this->set('SelectionLists',$this->SelectionLists->find('all'));
    $roles = $this->fetchTable('SelectionLists')->getNamesByDataId('1');
    $departments =$this->fetchTable('SelectionLists')->getNamesByDataId('2');
    $expertises = $this->fetchTable('SelectionLists')->getNamesByDataId('3');
@@ -236,7 +238,6 @@ class UsersController extends AppController
                     $user = $this->Users->patchEntity($user, $this->request->getData(), ['validate' => 'password']);
                     $user->password_reset_token = null;
                     $user->token_created_at = null;
-                    $user->updater_id = $user->id; 
 
                     if ($this->Users->save($user)) {
                         $this->Flash->success(__('パスワードが変更されました。'));
